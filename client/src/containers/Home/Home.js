4f750a5ddddb07cf47e1c0141fea9ded4d793f10
Guillaume Esnault <mon-compte1131@live.fr>
2019-02-06 15:48:55 +0100

    UseEffect

--- client/src/containers/Home/Home.js
+++ client/src/containers/Home/Home.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
   // Use state init a normal state with mutation func
   const [ count, setCount ] = useState(0);
 
+  // Listener, triggered when a hook is used
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
   return (
     <div>
-      <p>You clicked {count} times</p>
+      <p>You clicked {count} times
+      </p>
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
